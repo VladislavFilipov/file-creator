e904b98a3491ce622bc79d4d728320c0f10ebedb
Vladislav Filipov <vlad1philip@gmail.com>
2022-05-05 19:17:35 +0300

    one more fix (problem is old node version)

--- services/utils/prod-journal.utils.js
+++ services/utils/prod-journal.utils.js
@@ -3,7 +3,7 @@ const { sortArrayOfObjectsNew } = require("./common.utils");
 module.exports.addWorksToEmps = (employees, works) => {
 
     return employees.map(emp => {
-        let tmp = works.filter(work => work.employee?.id === emp.id).map(work => {
+        let tmp = works.filter(work => work.employee ? work.employee.id === emp.id : false).map(work => {
             let d = new Date();
             d.setFullYear(work.year);
             d.setMonth(work.month - 1);
@@ -18,7 +18,7 @@ module.exports.addWorksToEmps = (employees, works) => {
 module.exports.getEmpsByWorkshop = (employees) => {
     const result = {};
 
-    const filtered = employees.filter(emp => emp.dayToday?.length > 0);
+    const filtered = employees.filter(emp => emp.dayToday ? emp.dayToday.length > 0 : false);
 
     filtered.forEach(emp => {
         result[emp.workshop] ? result[emp.workshop].push(emp) : result[emp.workshop] = [emp];
